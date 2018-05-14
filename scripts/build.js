094c7c20133bb44eedfe8dc1e0ef4d9361209ad8
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2018-05-14 18:57:21 +0800

    优化构建步骤

--- scripts/build.js
+++ scripts/build.js
@@ -4,49 +4,55 @@ const fs = require('fs');
 const zlib = require('zlib');
 const uglify = require('uglify-js');
 
-const config = require('./config');
-
-
-[
+const defaultConfig = require('./config');
+const allConfig = [
   {}, {
     input: 'dist/Zen.js',
     output: 'dist/Zen.min.js'
   }
-].forEach((
-  {
-    input = config.input,
-    output = config.output.file
-  }
-) => {
-  
-  const isMinify = /min\.js$/.test( output );
-  const rollupConfig = extend(
-    {
-      input,
-      output: { file: output }
-    },
-    config
-  );
+];
 
-  rollup
-    .rollup( rollupConfig )
-    .then( bundle => {
-      return bundle.generate( rollupConfig.output )
-    })
-    .then( ({ code }) => {
-      if( isMinify ){
-        code = config.output.banner + '\n' + uglify.minify( code ).code;
-      }
-      return write( output, code );
-    })
-    .then(([ size, gzip ]) => {
-      console.log(`${ output } 已构建完毕!\n      size: ${ size }\n      gzip: ${ gzip }`);
-    })
-    .catch(
-      console.log.bind( console )
+!async function(){
+
+  for( let config of allConfig ){
+
+    const input = config.input || defaultConfig.input;
+    const output = config.output || defaultConfig.output.file;
+
+    const isMinify = /min\.js$/.test( output );
+    const rollupConfig = extend(
+      {
+        input,
+        output: { file: output }
+      },
+      defaultConfig
     );
 
-});
+    await new Promise(( resolve, reject ) => {
+
+      rollup
+        .rollup( rollupConfig )
+        .then( bundle => {
+          return bundle.generate( rollupConfig.output )
+        })
+        .then( ({ code }) => {
+          if( isMinify ){
+            code = defaultConfig.output.banner + '\n' + uglify.minify( code ).code;
+          }
+          return write( output, code );
+        })
+        .then( ([ size, gzip ]) => {
+          console.log(`${ output } 已构建完毕!\n      size: ${ size }\n      gzip: ${ gzip }`);
+          resolve();
+        })
+        .catch( error => {
+          console.log( error );
+          reject();
+        });
+    });
+  }
+}();
+
 
 function write( output, code ){
   return new Promise(( resolve, reject ) => {
