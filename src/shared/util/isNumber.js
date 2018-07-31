c2a38ec537789844b39296cdcdcd8567016f8fa3
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2018-07-31 10:13:47 +0800

    工具方法增加 $isNumber 传入参数是否是数字

--- src/shared/util/isNumber.js
+++ src/shared/util/isNumber.js
@@ -3,6 +3,17 @@
  * @param {any} obj 需要判断的对象
  * @returns {Boolean}
  */
-export default function isNumber( obj ){
+export function isNumber( obj ){
   return typeof obj === 'number' && obj === obj && isFinite( obj );
+}
+
+
+/**
+ * 判断传入的对象是否是数字
+ * @param {any} obj 需要判断的对象
+ * @returns {Boolean}
+ */
+export function $isNumber( obj ){
+  if( isNumber( obj ) ) return true;
+  return isString( obj ) && !isNaN( obj - parseFloat( obj ) );
 }
