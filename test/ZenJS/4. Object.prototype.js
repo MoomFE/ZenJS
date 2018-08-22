7752b3267f70d707fb02919e55ab1ae6bb5ded67
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2018-08-22 11:51:56 +0800

    删除历史遗留注释

--- test/ZenJS/4. Object.prototype.js
+++ test/ZenJS/4. Object.prototype.js
@@ -148,45 +148,5 @@ describes.push({
         }
       ]
     }
-
-
-    //   name: '$deleteValue',
-    //   it: function(){
-    //     var test = { z: 1, e: 2, n: 3, j: 4, s: 4 };
-
-    //     JSON
-    //       .stringify(
-    //         test.$deleteValue( 4 )
-    //       )
-    //       .should.equals(
-    //         '{"z":1,"e":2,"n":3}'
-    //       );
-    //   }
-    // }, {
-    //   name: '$get',
-    //   it: function(){
-    //     var test = { z: 1, e: 2, n: 3 };
-
-    //     test.$get( 'z' ).should.equals( 1 );
-    //     test.$get( 'e' ).should.equals( 2 );
-    //     test.$get( 'n' ).should.equals( 3 );
-    //   }
-    // }, {
-    //   name: '$set',
-    //   it: function(){
-    //      var test = {};
-
-    //      test.$set( 'ZenJS', 'Zw' )[ 'ZenJS' ].should.equals( 'Zw' );
-    //      test.$set( 1, 2 )[ 1 ].should.equals( 2 );
-    //   }
-    // }, {
-    //   name: '$self',
-    //   it: function(){
-    //     var test = {};
-
-    //     test.$self().should.equals( test );
-    //     test.__self__.should.equals( test );
-    //   }
-    // }
   ]
 });
