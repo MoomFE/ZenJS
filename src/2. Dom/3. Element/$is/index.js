4d1b92af7d08049d1b7895135335eaee1b097b3b
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2018-09-06 14:30:58 +0800

    修改 Element.prototype.matches 的 polyfill

--- src/2. Dom/3. Element/$is/index.js
+++ src/2. Dom/3. Element/$is/index.js
@@ -1,15 +1,15 @@
+import "../../../shared/global/DomElement/prototype/matches";
 import inBrowser from "../../../shared/const/inBrowser";
 import defineValue from "../../../shared/util/defineValue";
 import ElementProto from "../../../shared/global/DomElement/prototype/index";
 import isString from "../../../shared/util/isString";
-import matches from "../../../shared/global/DomElement/prototype/matches";
 
 
 if( inBrowser ){
 
   defineValue( ElementProto, '$is', function( selector ){
     return selector.nodeType ? this === selector
-                             : isString( selector ) ? matches.call( this, selector )
+                             : isString( selector ) ? this.matches( selector )
                                                     : false;
   });
 
--- src/shared/polyfill/Element.prototype.matches.js
+++ src/shared/polyfill/Element.prototype.matches.js
@@ -1,18 +1,15 @@
 import inBrowser from "../const/inBrowser";
 import ElementProto from "../global/DomElement/prototype/index";
 
-let matches;
 
-if( inBrowser ){
-  ( matches = ElementProto.matches ) || [ 'webkit', 'o', 'ms', 'moz' ].$each( core => {
+if( inBrowser && !ElementProto.matches ){
+  [ 'webkit', 'o', 'ms', 'moz' ].$each( core => {
     const matchesKey = core + 'MatchesSelector';
     const matchesValue = ElementProto[ matchesKey ];
 
     if( matchesValue ){
-      matches = matchesValue;
+      ElementProto.matches = matchesValue;
       return false;
     }
   });
 }
-
-export default matches;
